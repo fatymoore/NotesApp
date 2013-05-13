d0995a5443d9ffe79245f8ee7275e9de5fdf9007
Albert Asensio Gimenez <aasensiog@ymail.com>
2013-05-13 19:42:39 +0200

    added main js which fill the main list

---
+++ js/app.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function() {
+	//var notes = JSON.parse(localStorage['notesList']),
+	var note = null;
+
+	var notes = [{
+		title: 'title 1',
+		description: 'description1'
+	}, {
+		title: 'title 2',
+		description: 'description 2'
+	}];
+	for (var i = 0, l = notes.length; i < l; i++) {
+		note = notes[i];
+		$('#main-article>ul').append("<li class='arrow'><b>"+note.title+"</b><p>"+note.description+"</p></li>");
+	}
+});
