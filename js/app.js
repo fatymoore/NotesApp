997d40c17183edc2044bbebe3328f7da47755025
Albert Asensio Gimenez <aasensiog@ymail.com>
2013-05-14 17:04:54 +0200

    refresh after save

--- js/app.js
+++ js/app.js
@@ -1,41 +1,3 @@
-
-$(document).ready(function() {
-	//var notes = JSON.parse(localStorage['notesList']),
-	/*var note = null;
-
-	var notes = [{
-		title: 'title 1',
-		description: 'description1'
-	}, {
-		title: 'title 2',
-		description: 'description 2'
-	}];
-*/
-	/*
-	for (var i = 0, l = notes.length; i < l; i++) {
-		note = notes[i];
-		$('#main-article>ul').append("<li class='arrow'><b>"+note.title+"</b><p>"+note.description+"</p></li>");
-	}
-	*/
-
-	var messageList = JSON.parse(localStorage.getItem('notesList'));
-	if (messageList != null)
-	{
-		for (i = 0; i < messageList.length; i++) {
-			var messageItem = messageList[i];
-			/*
-			var template = _.template($('#tmpl_messages_list').html(), messageItem);
-			$('#main-article>ul').append(template);
-			*/
-			$('#main-article>ul').append(""+
-				"<li class='arrow'><strong>"+messageItem.title+"</strong>"+
-				"<p>"+messageItem.description+"</p>"+
-				"<p>"+messageItem.priority+"</p>"+
-				"</li>");
-		}
-	}
-});
-
 var saveNote = function() {
 	//TODO: get inputs type, description, priority
 	var title, message, priority, note, notesList, list;
@@ -57,3 +19,57 @@ var saveNote = function() {
  	list = JSON.stringify(notesList);
 	localStorage.setItem('notesList', list);
 };
+
+$(document).ready(function() {
+
+	var updateList = function() {
+		var messageList = JSON.parse(localStorage.getItem('notesList'));
+		if (messageList != null)
+		{
+			$('#main-article>ul').html('');
+			for (i = 0; i < messageList.length; i++) {
+				var messageItem = messageList[i];
+				/*
+				var template = _.template($('#tmpl_messages_list').html(), messageItem);
+				$('#main-article>ul').append(template);
+				*/
+
+				$('#main-article>ul').append(""+
+					"<li class='arrow'><strong>"+messageItem.title+"</strong>"+
+					"<p>"+messageItem.description+"</p>"+
+					"<p>"+messageItem.priority+"</p>"+
+					"</li>");
+			}
+		}
+	};
+	//var notes = JSON.parse(localStorage['notesList']),
+	/*var note = null;
+
+	var notes = [{
+		title: 'title 1',
+		description: 'description1'
+	}, {
+		title: 'title 2',
+		description: 'description 2'
+	}];
+*/
+	/*
+	for (var i = 0, l = notes.length; i < l; i++) {
+		note = notes[i];
+		$('#main-article>ul').append("<li class='arrow'><b>"+note.title+"</b><p>"+note.description+"</p></li>");
+	}
+	*/
+
+	$('#save').click(function() {
+		saveNote();
+		updateList();
+	});
+/*
+	$('.check').click(function() {
+		saveNote();
+		updateList();
+	});
+*/
+	updateList();
+});
+
