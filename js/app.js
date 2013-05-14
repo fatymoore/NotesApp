8051e70d60fc73666f0f16cd307a9b2a75bff070
Sonia Segura Romo <soniasr@tid.es>
2013-05-14 15:51:54 +0200

    Add save note

--- js/app.js
+++ js/app.js
@@ -10,23 +10,39 @@ $(document).ready(function() {
 		title: 'title 2',
 		description: 'description 2'
 	}];
-	
+
 	/*
 	for (var i = 0, l = notes.length; i < l; i++) {
 		note = notes[i];
 		$('#main-article>ul').append("<li class='arrow'><b>"+note.title+"</b><p>"+note.description+"</p></li>");
 	}
 	*/
-	
-	var messageList = Storage.getMessagesList();
+
+	var messageList = JSON.parse(localStorage.get('notesList'));
 
 	for (i = 0; i < messageList.length; i++) {
 		var messageItem = messageList[i];
-		var template = _.template($('#tmpl_foursquare_list').html(), messageItem);
-		$('#main-article').append(template);             
+		var template = _.template($('#tmpl_messages_list').html(), messageItem);
+		$('#main-article').append(template);
 	}
 });
 
 var saveNote = function() {
 	//TODO: get inputs type, description, priority
+	var title, message, priority, note, notesList, list;
+	title = $('#title').val();
+	message = $('#message').val();
+	priority = $('#priority').val();
+
+	note = {
+		title: title,
+		description: message,
+		priority: priority
+	};
+
+	notesList = JSON.parse(localStorage.get('notesList'));
+	notesList.push(note);
+ 	list = JSON.stringify(notesList);
+	localStorage.save('notesList', list);
+
 };
